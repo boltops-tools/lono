0d50b4587ecde314e7fd50c323dc2f9520643ddc
Tung Nguyen <tongueroo@gmail.com>
2017-06-05 21:09:17 -0700

    update are you sure delete message

--- lib/lono/cfn/util.rb
+++ lib/lono/cfn/util.rb
@@ -7,7 +7,7 @@ module Lono::Cfn::Util
       when :update
         "Are you sure you want to want to update the stack with the changes? (y/N)"
       when :delete
-        "Are you sure you want to want to delete the stack with the changes? (y/N)"
+        "Are you sure you want to want to delete the stack? (y/N)"
       end
       puts message
       sure = $stdin.gets
