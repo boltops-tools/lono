3748001d3d2d2c373c01777b715c419776041207
Tung Nguyen <tongueroo@gmail.com>
2018-02-07 18:32:50 -0800

    fix cfn_spec

--- spec/lib/lono/cfn_spec.rb
+++ spec/lib/lono/cfn_spec.rb
@@ -2,37 +2,38 @@ require "spec_helper"
 
 describe Lono::Cfn do
   before(:all) do
+    @env = "LONO_ROOT=#{ENV['LONO_ROOT']}"
     @args = "--noop"
   end
 
   describe "lono cfn" do
     it "create stack" do
-      out = execute("bin/lono cfn create my-stack #{@args}")
+      out = execute("#{@env} bin/lono cfn create my-stack #{@args}")
       expect(out).to include("Creating")
     end
 
     it "update stack" do
-      out = execute("bin/lono cfn update my-stack #{@args}")
+      out = execute("#{@env} bin/lono cfn update my-stack #{@args}")
       expect(out).to include("Updating")
     end
 
     it "delete stack" do
-      out = execute("bin/lono cfn delete my-stack #{@args}")
+      out = execute("#{@env} bin/lono cfn delete my-stack #{@args}")
       expect(out).to include("Deleted")
     end
 
     it "preview stack" do
-      out = execute("bin/lono cfn preview my-stack #{@args}")
+      out = execute("#{@env} bin/lono cfn preview my-stack #{@args}")
       expect(out).to include("CloudFormation preview")
     end
 
     it "diff stack" do
-      out = execute("bin/lono cfn diff my-stack #{@args}")
+      out = execute("#{@env} bin/lono cfn diff my-stack #{@args}")
       expect(out).to include("diff")
     end
 
     it "download stack" do
-      out = execute("bin/lono cfn download my-stack #{@args}")
+      out = execute("#{@env} bin/lono cfn download my-stack #{@args}")
       expect(out).to include("Download")
     end
   end
