21def5097bddc2505adca4b696ddb1d099e506f8
Tung Nguyen <tongueroo@gmail.com>
2018-02-14 16:54:56 -0800

    cleanup

--- lib/lono/template/template.rb
+++ lib/lono/template/template.rb
@@ -6,6 +6,13 @@ class Lono::Template::Template
   include Lono::CurrentRegion
   include ERB::Util
 
+  # Main template DSL methods are: source and variables
+  #
+  #   template "example-2" do
+  #     source "example"
+  #     variables(test: 1)
+  #   end
+  #
   def initialize(name, block=nil, options={})
     # Taking care to name instance variables with _ in front because we load the
     # variables from config/variables and those instance variables can clobber these
@@ -13,18 +20,13 @@ class Lono::Template::Template
     @name = name
     @block = block
     @options = options
-    @source = default_source(name)
+    @source_path = default_source_path(name)
   end
 
-  # Main template DSL methods.
-  #
-  #   template "example-2" do
-  #     source "example"
-  #     variables(test: 1)
-  #   end
+  # Returns path, example: ./app/templates/example.yml
   def source(path)
-    @source = path[0..0] == '/' ? path : "#{Lono.config.templates_path}/#{path}"
-    @source += ".yml"
+    @source_path = path[0..0] == '/' ? path : "#{Lono.config.templates_path}/#{path}"
+    @source_path += ".yml"
   end
 
   def variables(vars={})
@@ -34,16 +36,17 @@ class Lono::Template::Template
   end
 
   # internal methods
-  def default_source(name)
+  def default_source_path(name)
     "#{Lono.config.templates_path}/#{name}.yml" # defaults to name, source method overrides
   end
 
   def build
     instance_eval(&@block) if @block
 
-    # template = Tilt::ERBTemplate.new(@source)
+    # template = Tilt::ERBTemplate.new(@source_path)
     # template.render(context)
-    RenderMePretty.result(@source, context: context)
+    puts "@source_path #{@source_path.inspect}"
+    RenderMePretty.result(@source_path, context: context)
   end
 
   # Context for ERB rendering.
