038e9372915504a7fce99c4afe4c967040244e39
Tung Nguyen <tongueroo@gmail.com>
2018-02-15 13:49:19 -0800

    upgrade settings format to support multiple LONO_ENV

--- lib/lono/setting.rb
+++ lib/lono/setting.rb
@@ -1,45 +1,48 @@
-class Lono::Setting
-  # The options from the files get merged with the following precedence:
-  #
-  # current folder - The current folder’s config/settings.yml values take the highest precedence.
-  # user - The user’s ~/.lono/settings.yml values take the second highest precedence.
-  # default - The default settings bundled with the tool takes the lowest precedence.
-  #
-  # More info: http://lono.cloud/docs/settings/
-  def data
-    return @settings_yaml if @settings_yaml
+require 'yaml'
 
-    project_file = "#{Lono.root}/config/settings.yml"
-    project = File.exist?(project_file) ? YAML.load_file(project_file) : {}
+module Lono
+  class Setting
+    def initialize(check_lono_project=true)
+      @check_lono_project = check_lono_project
+    end
 
-    user_file = "#{ENV['HOME']}/.lono/settings.yml"
-    user = File.exist?(user_file) ? YAML.load_file(user_file) : {}
+    # data contains the settings.yml config.  The order or precedence for settings
+    # is the project lono/settings.yml and then the ~/.lono/settings.yml.
+    def data
+      return @settings_yaml if @settings_yaml
 
-    default_file = File.expand_path("../default/settings.yml", __FILE__)
-    default = YAML.load_file(default_file)
+      if @check_lono_project && !File.exist?(project_settings_path)
+        puts "ERROR: No settings file at #{project_settings_path}.  Are you sure you are in a project with lono setup?"
+        exit 1
+      end
 
-    @settings_yaml = default.merge(user.merge(project))
-  end
+      # project based settings files
+      project = load_file(project_settings_path)
 
-  # Examples:
-  #
-  # Using the Lono.env
-  # s3:
-  #   path:
-  #     production: s3://infrastructure-prod/cloudformation
-  #     staging: s3://infrastructure-dev/cloudformation
-  #
-  # Using the AWS_PROFILE
-  # s3:
-  #   path:
-  #     my-prod-profile: s3://infrastructure-prod/cloudformation
-  #     my-stag-profile: s3://infrastructure-dev/cloudformation
-  #
-  def s3_path
-    s3 = data['s3']
-    s3_path = s3['path']
-    # s3_path['default'] - key will always exist because of default lono/settings.yml
-    #   default value is nil though
-    s3_path[ENV['AWS_PROFILE']] || s3_path[Lono.env] || s3_path['default']
+      user_file = "#{ENV['HOME']}/.lono/settings.yml"
+      user = File.exist?(user_file) ? YAML.load_file(user_file) : {}
+
+      default_file = File.expand_path("../default/settings.yml", __FILE__)
+      default = YAML.load_file(default_file)
+
+      @settings_yaml = default.deep_merge(user.deep_merge(project))[Lono.env]
+    end
+
+  private
+    def load_file(path)
+      content = RenderMePretty.result(path)
+      data = File.exist?(path) ? YAML.load(content) : {}
+      # automatically add base settings to the rest of the environments
+      data.each do |lono_env, _setting|
+        base = data["base"] || {}
+        env = data[lono_env] || {}
+        data[lono_env] = base.merge(env) unless lono_env == "base"
+      end
+      data
+    end
+
+    def project_settings_path
+      "#{Lono.root}/config/settings.yml"
+    end
   end
 end
