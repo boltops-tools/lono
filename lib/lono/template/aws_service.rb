89d9b642f389ba91e8b68ce14871b8e96fd8e3d7
Tung Nguyen <tongueroo@gmail.com>
2018-02-25 06:22:00 -0800

    update lono cfn diff docs, add S3_REGION env option to configure different s3 client region vs AWS_PROFILE

--- lib/lono/template/aws_service.rb
+++ lib/lono/template/aws_service.rb
@@ -7,6 +7,9 @@ module Lono::Template::AwsService
     options = {}
     # example: endpoint: 'https://s3.us-west-2.amazonaws.com'
     options[:endpoint] = ENV['S3_ENDPOINT'] if ENV['S3_ENDPOINT']
+    # allow override of region for s3 client to avoid warning:
+    # S3 client configured for "us-east-1" but the bucket "xxx" is in "us-west-2"; Please configure the proper region to avoid multiple unnecessary redirects and signing attempts
+    options[:region] = ENV['S3_REGION'] if ENV['S3_REGION']
     @s3 = Aws::S3::Client.new(options)
   end
 end
