11258630237c72647d34528290b16277261e2146
Tung Nguyen <tongueroo@gmail.com>
2019-10-08 17:28:32 +0000

    filter github webhook to only build on push event
    * dont want 2 builds to start for pull requests

--- .cody/project.rb
+++ .cody/project.rb
@@ -1,3 +1,6 @@
 github_url("https://github.com/tongueroo/lono")
 linux_image("aws/codebuild/ruby:2.5.3-1.7.0")
-triggers(webhook: true)
+triggers(
+  webhook: true,
+  filter_groups: [[{type: "EVENT", pattern: "PUSH"}]]
+)
