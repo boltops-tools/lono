f18a6ebd44179a199aff197c68fb3b5d2e43563d
Tung Nguyen <tongueroo@gmail.com>
2019-12-10 04:58:41 +0000

    fix md5 sum calcuation for single file

--- lib/lono/md5.rb
+++ lib/lono/md5.rb
@@ -15,19 +15,28 @@ module Lono
       # This checksum within the file name is to mainly make sure that Lambda::Function resources "change" and an update is triggered.
       # There's another checksum in the upload code that makes sure we don't upload the code again to speed up things.
       def sum(path)
-        files = Dir["#{path}/**/*"]
-        files = files.reject { |f| File.directory?(f) }
-                     .reject { |f| File.symlink?(f) }
-        files.sort!
-        content = files.map do |f|
-          Digest::MD5.file(f).to_s[0..7]
-        end.join
+        content = if File.directory?(path)
+          sum_directory(path)
+        else
+          Digest::MD5.file(path).to_s[0..7]
+        end
 
         md5 = Digest::MD5.new
         md5.update(content)
         md5.hexdigest.to_s[0..7]
       end
       memoize :sum
+
+      def sum_directory(dir)
+        files = Dir["#{dir}/**/*"]
+        files = files.reject { |f| File.directory?(f) }
+                     .reject { |f| File.symlink?(f) }
+        files.sort!
+
+        files.map do |f|
+          Digest::MD5.file(f).to_s[0..7]
+        end.join
+      end
 
       def name(path)
         relative_path = name_without_md5(path)
