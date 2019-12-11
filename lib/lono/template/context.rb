afba4758d13a597b9c1d222c987678dc597474c9
Tung Nguyen <tongueroo@gmail.com>
2019-12-11 01:41:36 +0000

    comments

--- lib/lono/template/context.rb
+++ lib/lono/template/context.rb
@@ -22,6 +22,7 @@ class Lono::Template
     end
 
     # For Lono::AppFile::Build usage of Thor::Action directory
+    # For some reason a send(:binding) doesnt work but get_binding like this works.
     def get_binding
       binding
     end
