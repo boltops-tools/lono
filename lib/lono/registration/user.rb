d16bde3d67a932b6fed6511bfb719660980d2b65
Tung Nguyen <tongueroo@gmail.com>
2020-01-17 03:04:43 +0000

    improve reg check message

--- lib/lono/registration/user.rb
+++ lib/lono/registration/user.rb
@@ -6,11 +6,16 @@ class Lono::Registration
       info = read_registration
       unless info
         say "Lono is not registered."
-        say "The .lono/registration.yml file does not exist."
+        say "The .lono/registration.yml and ~/.lono/registration.yml file does not exist."
+        return false
+      end
+      unless info[:content].is_a?(Hash)
+        say "Lono is not registered."
+        say "#{info[:path]} does not have the right structure"
         return false
       end
 
-      @resp = request_verification(info)
+      @resp = request_verification(info[:content])
       # A non-200 response means there was a non-200 http response. Failsafe behavior is to continue.
       # Unless called from the cli: lono registration check
       if @resp.nil?
@@ -28,7 +33,7 @@ class Lono::Registration
       end
 
       if @resp[:message]
-        say "Lono is not correctly registered. Unable to confirm info in #{@found}"
+        say "Lono is not correctly registered. Unable to confirm info in #{info[:path]}"
         say @resp[:message]
       end
       false
@@ -37,18 +42,19 @@ class Lono::Registration
     def read_registration
       folders = [Lono.root, ENV['HOME']]
       files = folders.map { |f| "#{f}/.lono/registration.yml" }
-      @found = files.find do |path|
+      found = files.find do |path|
         File.exist?(path)
       end
-      return unless @found
+      return unless found
 
-      content = RenderMePretty.result(@found, context: self)
+      content = RenderMePretty.result(found, context: self)
       if @options[:debug]
         puts "Debug mode enabled. Here's the lono registration info being used:"
+        puts "Using: #{found}"
         puts content
         puts
       end
-      YAML.load(content)
+      {path: found, content: YAML.load(content)}
     end
   end
 end
