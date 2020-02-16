7a05e6b3f2eca54f6d16fecd765f1e18932c5cb9
Tung Nguyen <tongueroo@gmail.com>
2020-02-16 18:17:34 +0000

    add lono_type metadata concept

--- lib/lono/template/generator.rb
+++ lib/lono/template/generator.rb
@@ -6,7 +6,7 @@ class Lono::Template
       # Examples:
       #   Erb.new(options).run
       #   Dsl.new(options).run
-      generator_class = "Lono::Template::Strategy::#{template_type.camelize}"
+      generator_class = "Lono::Template::Strategy::#{lono_strategy.camelize}"
       generator_class = Object.const_get(generator_class)
       generator_class.new(@options).run
       # The generator strategy class writes template to disk. The inject_configsets reads it back from disk.
@@ -14,12 +14,12 @@ class Lono::Template
       inject_configsets
     end
 
-    def template_type
+    def lono_strategy
       if @options[:source]
         "source"
       else
-        jadespec = Lono::Jadespec.new(Lono.blueprint_root, "unknown") # abusing Jadespec to get template_type
-        jadespec.template_type
+        jadespec = Lono::Jadespec.new(Lono.blueprint_root, "unknown") # abusing Jadespec to get lono_strategy
+        jadespec.lono_strategy
       end
     end
 
