c584b5b30b579e13e5b769fbbfe3f14c03997d3e
Tung Nguyen <tongueroo@gmail.com>
2020-04-08 20:47:39 +0000

    fix materializer for multiple extensions

--- lib/lono/extensions/preparer.rb
+++ lib/lono/extensions/preparer.rb
@@ -7,16 +7,20 @@ class Lono::Extensions
 
     def run
       @register.run
-      materialize
-      # loading happens in Lono::Extensions::Loader load_all_extension_helpers module
-      # so it has the proper self.class include scope
+      download
+      final_materialize
     end
 
-    def materialize
+    def download
       Lono::Jade::Registry.tracked_extensions.each do |registry|
         jade = Lono::Jade.new(registry.name, "extension", registry)
-        jade.materialize
+        jade.materialize # adds to Lono::Jade::Registry.downloaded_extensions
       end
+    end
+
+    def final_materialize
+      jades = Lono::Jade::Registry.downloaded_extensions
+      Lono::Jade::Materializer::Final.new.build(jades)
     end
   end
 end
